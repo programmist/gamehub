a7b45eaed1e353541d29a9f82427f3d423ab96a6
Tony Childs <tony.childs@gmail.com>
2023-09-02 19:36:23 -0500

    Add game grid, api client, and make initial http call

---
+++ src/services/api-client.ts
@@ -0,0 +1,10 @@
+import axios, { CanceledError } from "axios";
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: import.meta.env.VITE_RAWG_API_KEY,
+  },
+});
+
+export { CanceledError };
