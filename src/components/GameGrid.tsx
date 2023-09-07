c3fc576922b7457d1a8c04515f24f6d32cd03252
Tony Childs <tony.childs@gmail.com>
2023-09-06 22:55:42 -0500

    Update title and use static data (test vercel auto-deploy

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 function GameGrid({ query }: Props) {
-  const { data: games, error, isLoading } = useGames(query);
+  const { data: games, error, isLoading } = useGames(query, false);
   const skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
