d8e047ca7d024a00d4b064cb08fe37dcd3c8e1c1
Tony Childs <tony.childs@gmail.com>
2023-09-12 18:25:34 -0500

    Watch gameQuery instead of params.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,20 +3,20 @@ import { FetchResponse } from "../services/api-client";
 import gameService, { Game, GameQuery } from "../services/game-service";
 import { useQuery } from "@tanstack/react-query";
 
-const useGames = (query: GameQuery, useLiveData = true) => {
+const useGames = (gameQuery: GameQuery, useLiveData = true) => {
   const staticData = { count: staticGames.length, results: staticGames };
-  const params = {
-    params: {
-      genres: query?.genre?.id,
-      parent_platforms: query?.platform?.id,
-      ordering: query.order.value,
-      search: query?.search,
-    },
-  };
   return useLiveData
     ? useQuery<FetchResponse<Game>, Error>({
-        queryKey: ["games", params],
-        queryFn: () => gameService.getAll(params),
+        queryKey: ["games", gameQuery],
+        queryFn: () =>
+          gameService.getAll({
+            params: {
+              genres: gameQuery?.genre?.id,
+              parent_platforms: gameQuery?.platform?.id,
+              ordering: gameQuery.order.value,
+              search: gameQuery?.search,
+            },
+          }),
         staleTime: 5 * 60 * 1000, // 5 minutes
       })
     : {
