adee50e74cf72d8b2010191dd6da88cb56ca6be0
Tony Childs <tony.childs@gmail.com>
2023-09-13 17:47:49 -0500

    update static data to full response object

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -4,16 +4,11 @@ import { FetchResponse } from "../services/api-client";
 import staticGenres from "../data/genres";
 
 const useGenres = () => {
-  const staticData = {
-    count: staticGenres.length,
-    next: null,
-    results: staticGenres,
-  };
   return useQuery<FetchResponse<Genre>, Error>({
     queryKey: ["genres"],
     queryFn: genreService.getAll,
     staleTime: 24 * 60 * 60 * 1000, // one day
-    initialData: staticData,
+    initialData: staticGenres,
   });
 };
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -4,16 +4,11 @@ import platformService, { Platform } from "../services/platform-service";
 import { useQuery } from "@tanstack/react-query";
 
 const usePlatforms = () => {
-  const staticData = {
-    count: staticPlatforms.length,
-    next: null,
-    results: staticPlatforms,
-  };
   return useQuery<FetchResponse<Platform>, Error>({
     queryKey: ["platforms"],
     queryFn: platformService.getAll,
     staleTime: 24 * 60 * 60 * 1000, // one day
-    initialData: staticData,
+    initialData: staticPlatforms,
   });
 };
 
