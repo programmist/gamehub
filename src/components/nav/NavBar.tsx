1ee33d4fbdc2070e2d0163cbbe0aacfdbdacca6b
Tony Childs <tony.childs@gmail.com>
2023-09-17 11:56:59 -0500

    Add home link to navbar icon

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link to="/" style={{ display: "inline" }}>
+        <Image src={logo} boxSize="60px" maxWidth={100} />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
